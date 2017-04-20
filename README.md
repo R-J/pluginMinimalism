2c6955fd65357479fe7269aeff30a10371c61bec
r-j <robin.jurinka@gmx.net>
2017-04-20 10:36:20 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# hollowPlugin
-Plugin for Vanilla forums which contains only the bare minimum of a plugin and has no functionality at all
+# pluginMinimalism
+Plugin for Vanilla forums which contains only the bare minimum of a plugin and has no functionality at all. You can use this as a quick starter for your own projects.
---
+++ class.pluginminimalism.plugin.php
@@ -0,0 +1,37 @@
+<?php
+
+$PluginInfo['pluginMinimalism'] = [
+    'Name' => 'Plugin Minimalism',
+    'Description' => 'Minimal plugin for writing your own plugin from scratch.',
+    'Version' => '0.0.1',
+    'Author' => 'Robin Jurinka',
+    /*
+    'AuthorUrl' => 'https://open.vanillaforums.com/profile/r_j',
+    'RequiredApplications' => ['Vanilla' => '>=2.3'],
+    'RegisterPermissions' => ['PluginMinimalism.Manage'],
+    'SettingsUrl' => 'settings/pluginminimalism',
+    'RequiredTheme' => false,
+    'RequiredPlugins' => false,
+    'HasLocale' => true,
+    'MobileFriendly' => true,
+    */
+    'License' => 'MIT'
+];
+
+class PluginMinimalismPlugin extends Gdn_Plugin {
+    /*
+    public function setup() {
+        $this->structure();
+    }
+    */
+
+    /*
+    public function structure() {
+    }
+    */
+
+    /*
+    public function onDisable() {
+    }
+    */
+}
